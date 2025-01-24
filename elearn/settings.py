5fe73bfb537562ab2c2cb72eaeba725ebc62b81b
NortonFire007 <rioknuto@gmail.com>
2025-01-24 19:14:36 +0200

    ✨🗄️Add Redis cache backend & Redisboard monitoring 📊

--- elearn/settings.py
+++ elearn/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'students.apps.StudentsConfig',
     'embed_video',
     'debug_toolbar',
+    'redisboard',
 ]
 
 MIDDLEWARE = [
@@ -96,8 +97,10 @@ DATABASES = {
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+        # 'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        # 'LOCATION': '127.0.0.1:11211',
     }
 }
 
