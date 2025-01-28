364e4c05606949f4ca953c46a70f5262b501a78d
NortonFire007 <rioknuto@gmail.com>
2025-01-28 21:21:07 +0200

    🔧 Refactor project structure: reorganized folders for better maintainability

--- elearn/elearn/settings/base.py
+++ elearn/elearn/settings/base.py
@@ -26,9 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -90,20 +87,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'elearn.wsgi.application'
 ASGI_APPLICATION = 'elearn.asgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
 
 CACHES = {
     'default': {
---
+++ elearn/elearn/settings/prod.py
@@ -0,0 +1,29 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = [
+    (os.getenv('ADMIN_NAME'), os.getenv('ADMIN_EMAIL')),
+]
+
+ALLOWED_HOSTS = ['elernproject25.com', 'www.elernproject25.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+
+# Security
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
